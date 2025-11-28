effa9709a8ad1fd04317c223d48c45893373676e
Eric Prototype <ericprottung@gmail.com>
2025-11-28 13:23:33 +0100

    ex02 working

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: eprottun <eprottun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 14:53:45 by eprottun          #+#    #+#             */
-/*   Updated: 2025/11/27 17:07:20 by eprottun         ###   ########.fr       */
+/*   Updated: 2025/11/28 13:11:14 by eprottun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,33 @@ class Fixed {
 		Fixed(const float value);
 		Fixed(const Fixed& other);
 		Fixed& operator=(const Fixed& other);
-		bool operator<(const Fixed& other);
-		bool operator>(const Fixed& other);
-		bool operator==(const Fixed& other);
-		bool operator>=(const Fixed& other);
-		bool operator<=(const Fixed& other);
-		bool operator!=(const Fixed& other);
-		const Fixed& operator++();
-		const Fixed operator++(int);
-		const Fixed& operator--();
-		const Fixed operator--(int);
+		bool operator<(const Fixed& other) const;
+		bool operator>(const Fixed& other) const;
+		bool operator==(const Fixed& other) const;
+		bool operator>=(const Fixed& other) const;
+		bool operator<=(const Fixed& other) const;
+		bool operator!=(const Fixed& other) const;
+		Fixed& operator++();
+		Fixed operator++(int);
+		Fixed& operator--();
+		Fixed operator--(int);
 		Fixed operator+(const Fixed& other) const;
-		Fixed operator-(const Fixed& other);
-		Fixed operator*(const Fixed& other);
-		Fixed operator/(const Fixed& other);
+		Fixed operator-(const Fixed& other) const;
+		Fixed operator*(const Fixed& other) const ;
+		Fixed operator/(const Fixed& other) const ;
 		~Fixed();
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
 		float toFloat( void ) const;
 		int toInt( void ) const;
-		const int& min(const Fixed& nr1, const Fixed& nr2);
+		static const Fixed& min(const Fixed& nr1, const Fixed& nr2);
+		static Fixed& min(Fixed& nr1, Fixed& nr2);
+		static const Fixed& max(const Fixed& nr1, const Fixed& nr2);
+		static Fixed& max(Fixed& nr1, Fixed& nr2);
 	private :
 		int fp_number;
 		static const int fractional_bits = 8;
-	}	;
-	
+}	;
+
+
 #endif
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eprottun <eprottun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 14:56:31 by eprottun          #+#    #+#             */
-/*   Updated: 2025/11/27 16:16:54 by eprottun         ###   ########.fr       */
+/*   Updated: 2025/11/28 13:07:16 by eprottun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,33 @@ std::ostream& operator<<(std::ostream& os, Fixed const& fixed)
 }
 	
 int main( void ) {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	return 0;
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+std::cout << Fixed::min( a, b ) << std::endl;
+std::cout << Fixed::min( --a, b ) << std::endl;
+std::cout << Fixed::min( ++a, b ) << std::endl;
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << a << " is a" << std::endl;
+std::cout << b << " is b" << std::endl;
+std::cout << Fixed::min( a, b) << " is min" << std::endl;
+std::cout << std::endl;
+std::cout << std::endl;
+Fixed c(5);
+c = c * Fixed(10);
+c = c * Fixed(10);
+std::cout << c << " is c" << std::endl;
+c = c / Fixed(10);
+c = c / Fixed(10);
+std::cout << c << " is c" << std::endl;
+return 0;
 }
